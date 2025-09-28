99ec81b46cc46556a6aa6f79fdbcaebb26ff4192
John Waczak <john.waczak@gartner.com>
2025-09-28 15:59:05 -0500

    initial commit

---
+++ src/sklatent/__init__.py
@@ -0,0 +1,13 @@
+"""
+Scikit-learn compatible python package for latent-space models.
+
+- **NMF**: Non-negative Matrix Factorization
+- **SOM**: Self-Organizing Maps
+- **GTM**: Generative Topographic Mapping
+- **GSM**: Generative Simplex Mapping
+"""
+
+
+def main() -> None:
+    """Test out the package functionality by printing a simple statement."""
+    print("Hello from scikit-latent!")
